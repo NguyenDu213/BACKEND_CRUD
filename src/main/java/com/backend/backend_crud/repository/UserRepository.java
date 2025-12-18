d6af5afd244f2d6340e657cdbbcf49d497acd465
DuyKhuong25 <dkhuong.2512@gmail.com>
2025-12-18 08:54:21 +0700

    add repository

---
+++ src/main/java/com/backend/backend_crud/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.backend.backend_crud.repository;
+
+import com.backend.backend_crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
